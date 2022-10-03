07e539017b29655cfd6a5f88819767417948883b
swlws <979623074@qq.com>
2022-10-03 14:25:07 +0800

    feat: 目录

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,20 @@ import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 const Home = React.lazy(() => import("src/views/home/index"));
+const Voice = React.lazy(() => import("src/views/voice/index"));
+const Photo = React.lazy(() => import("src/views/photo/index"));
+const Video = React.lazy(() => import("src/views/video/index"));
+const Screen = React.lazy(() => import("src/views/screen/index"));
 
 export default function AppRouters() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/voice" element={<Voice />} />
+        <Route path="/photo" element={<Photo />} />
+        <Route path="/video" element={<Video />} />
+        <Route path="/screen" element={<Screen />} />
       </Routes>
     </Suspense>
   );
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,6 +1,21 @@
+import { NavLink } from "react-router-dom";
 import styles from "./index.module.scss";
 
 function Home() {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <article className={styles.container}>
+      <header>
+        <h2>媒体工具</h2>
+        <p>纯粹的WebApi调用，不涉及网络请求、不存储用户信息。</p>
+      </header>
+
+      <main>
+        <NavLink to="/voice">录制声音</NavLink>
+        <NavLink to="/photo">拍照</NavLink>
+        <NavLink to="/video">录制视频</NavLink>
+        <NavLink to="/screen">捕获屏幕</NavLink>
+      </main>
+    </article>
+  );
 }
 export default Home;
