2ef1791585a6ae24acecc74ad2f6cda026a08ecf
swlws <979623074@qq.com>
2022-10-04 17:13:46 +0800

    feat: video

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 媒体 API 操作，纯粹的前端工程，不记录用户信息、不发送 HTTP Request。
 
-## 录音
+## 录制声音
 
 客户端使用`navigator.mediaDevices.getUserMedia`获取的音频的`stream`，再使用`MediaRecorder`记录声音，最终使用`<audio>`播放。
 
@@ -10,3 +10,14 @@
 
 - 声音录制
 - 音频文件下载
+
+## 录制视频
+
+客户端使用`navigator.mediaDevices.getUserMedia`获取的音频、视频的`stream`，再使用`MediaRecorder`记录声音，最终使用`<video>`播放。
+
+能力：
+
+- 声音录制
+- 视频录制
+- 视频文件下载
+- 画中画
--- src/views/video/index.tsx
+++ src/views/video/index.tsx
@@ -1,3 +1,189 @@
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import styles from "./index.module.scss";
+
+function streamRecorder(mediaRecorder: MediaRecorder): Promise<Blob[]> {
+  return new Promise((resolve, reject) => {
+    let chunks: Blob[] = [];
+    mediaRecorder.onstart = function (e) {
+      chunks = [];
+    };
+    mediaRecorder.ondataavailable = function (e) {
+      chunks.push(e.data);
+    };
+    mediaRecorder.onstop = function (e) {
+      resolve(chunks);
+    };
+    mediaRecorder.start();
+  });
+}
+
+function useRecorder(
+  videoRef: MutableRefObject<any>
+): [Blob[], () => void, () => void] {
+  const [chunks, setChunks] = useState<Blob[]>([]);
+  let cacheStream: MediaStream, recorder: MediaRecorder;
+
+  const start = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        cacheStream = stream;
+
+        // 新建一个recorder
+        recorder = new MediaRecorder(stream);
+
+        // 实时记录音频、视频
+        const preview = videoRef.current;
+        preview.srcObject = stream;
+        preview.captureStream =
+          preview.captureStream || preview.mozCaptureStream;
+        return new Promise((resolve) => (preview.onplaying = resolve));
+      })
+      .then(() => {
+        // 启动记录
+        return streamRecorder(recorder);
+      })
+      .then((chunks) => {
+        console.log(chunks);
+        setChunks(chunks);
+      });
+  };
+
+  const stop = () => {
+    // 关闭音频、视频流
+    if (!!cacheStream) {
+      cacheStream.getTracks().forEach((track) => track.stop());
+    }
+    // 关闭recorder
+    if (!!recorder) {
+      recorder.stop();
+    }
+  };
+
+  return [chunks, start, stop];
+}
+
+type RecorderProps = { upload: (chunks: Blob[]) => void };
+function Recorder(props: RecorderProps) {
+  const vRef = useRef(null);
+
+  useEffect(() => {
+    const preview = vRef.current as any;
+    preview.autoplay = true;
+  }, []);
+
+  const [chunks, start, stop] = useRecorder(vRef);
+
+  useEffect(() => {
+    if (chunks.length === 0) return;
+
+    props.upload(chunks);
+  }, [chunks, props]);
+
+  return (
+    <article className={styles["block-layout"]}>
+      <header>录制视频</header>
+      <main>
+        <video ref={vRef} src="" muted></video>
+      </main>
+
+      <footer>
+        <button onClick={start}>开始录制</button>
+        <button onClick={stop}>停止录制</button>
+      </footer>
+    </article>
+  );
+}
+
+/**
+ * 画中画
+ *
+ * @param elRef
+ */
+function togglePictureInPicture(elRef: MutableRefObject<null>) {
+  if (document.pictureInPictureElement) {
+    document.exitPictureInPicture();
+  } else {
+    if (document.pictureInPictureEnabled) {
+      const ref = elRef.current as any;
+      ref.requestPictureInPicture();
+    }
+  }
+}
+
+type PlayerProps = { chunks: Blob[] };
+function Player(props: PlayerProps) {
+  const vRef = useRef(null);
+
+  const [ready, setReady] = useState(false);
+  const [url, setUrl] = useState<string>("");
+
+  const [info, setInfo] = useState({ size: 0, type: "" });
+  useEffect(() => {
+    const flag = props.chunks.length !== 0;
+    setReady(flag);
+
+    if (flag === false) return;
+
+    let blob = new Blob(props.chunks, {
+      type: "video/webm",
+    });
+    setUrl(URL.createObjectURL(blob));
+
+    setInfo({
+      size: blob.size,
+      type: blob.type,
+    });
+
+    // 设置自动播放
+    const ref = vRef.current as any;
+    ref.autoplay = flag;
+  }, [props]);
+
+  const footerEls = () => {
+    if (ready)
+      return (
+        <footer>
+          <button onClick={() => togglePictureInPicture(vRef)}>画中画</button>
+          <button>
+            <a href={url} download="video.webm">
+              下载
+            </a>
+          </button>
+
+          <p>
+            the video file {info.size} bytes of {info.type} media
+          </p>
+        </footer>
+      );
+
+    return null;
+  };
+
+  return (
+    <article className={styles["block-layout"]}>
+      <header>播放视频</header>
+
+      <main>
+        <video ref={vRef} src={url} controls={ready}></video>
+      </main>
+
+      {footerEls()}
+    </article>
+  );
+}
+
 export default function Video() {
-  return <>video</>;
+  const [data, setData] = useState<Blob[]>([]);
+  const setChunks = (chunks: Blob[]) => {
+    setData(chunks);
+  };
+
+  return (
+    <article className={styles.container}>
+      <Recorder upload={setChunks} />
+
+      <Player chunks={data} />
+    </article>
+  );
 }
